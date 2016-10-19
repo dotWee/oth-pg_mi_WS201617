623fe132a0c0c4ce0936a312d2ca6bb5dc8791e6
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2016-10-19 12:56:02 -0700

    Bessere Version von Rechner2 veröffentlicht

--- pg_mi_02_datentypenundkontrollstrukturen/rechner2.c
+++ pg_mi_02_datentypenundkontrollstrukturen/rechner2.c
@@ -5,16 +5,12 @@
 int main(void) {
   int response = 0;
   int z1 = 0, z2 = 0;
-  char op[10] = "";
+  char op = ' ';
   do {
-    printf("Zahl 1: ");
-    scanf("%d", &z1);
-    printf("Operator: ");
-    scanf("%s", op);
-    printf("Zahl 2: ");
-    scanf("%d", &z2);
+    printf("Zahl Operator Zahl eingeben: ");
+    scanf("%d %c %d", &z1, &op, &z2);
 
-    switch(op[0]) {
+    switch(op) {
       case '+':
         printf("%d + %d = %d\n", z1, z2, z1+z2);
         break;
