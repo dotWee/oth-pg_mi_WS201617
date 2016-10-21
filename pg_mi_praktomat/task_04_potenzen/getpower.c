e5ff854bd0c8b4ea4cfad6ef83d1576ed3b61263
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-10-21 21:21:22 +0200

    Simplify solution for task 04: Potenzen using switch/case

--- pg_mi_praktomat/task_04_potenzen/getpower.c
+++ pg_mi_praktomat/task_04_potenzen/getpower.c
@@ -14,25 +14,35 @@ int main(void) {
     scanf("%d", &exponent);
 
     // Ergebnisses von Typ double einfügen.
+    switch (exponent) {
 
-    if (exponent == -1) {
-        result = 0 - basis;
-    } else if (exponent == 0) {
-        result = 1;
-    } else if (exponent == 1) {
-        result = basis;
-    } else if (exponent > 1) {
-        result = 1;
+        case -1:
+            result = 0 - basis;
+            break;
 
-        for (int n = 1; n <= exponent; n++) {
-            result = result * basis;
-        }
-    } else if (exponent < -1) {
-        result = 1;
+        case 0:
+            result = 1;
+            break;
 
-        for (int n = 0; n > exponent; n--) {
-            result = result / basis;
-        }
+        case 1:
+            result = basis;
+            break;
+
+        default:
+            if (exponent > 1) {
+                result = 1;
+
+                for (int n = 1; n <= exponent; n++) {
+                    result = result * basis;
+                }
+            } else if (exponent < -1) {
+                result = 1;
+
+                for (int n = 0; n > exponent; n--) {
+                    result = result / basis;
+                }
+            }
+            break;
     }
 
     printf("Ergebnis: %lf\n", result);
