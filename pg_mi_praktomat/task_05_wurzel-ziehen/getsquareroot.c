9230c6309d36aa6062d32e22dcc79e0530274eb3
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-10-21 21:34:30 +0200

    Finalize solution for task 05: Wurzel ziehen

--- pg_mi_praktomat/task_05_wurzel-ziehen/getsquareroot.c
+++ pg_mi_praktomat/task_05_wurzel-ziehen/getsquareroot.c
@@ -9,6 +9,13 @@ int main(int argc, char *argv[]) {
     int zahl = atoi(argv[1]);
 
     // Hier bitte den Code zur Berechnung der Quadratwurzel einfügen
+    // Anwendung des Newton Verfahrens zur Berechnung der Quadratwurzel
+    double abweichung = 0.0000001;
+    double f1 = abweichung;
+
+    while (f1 * f1 <= zahl) {
+        f1 += abweichung;
+    }
 
     printf("Ergebnis: %lf\n", f1);
     return 0;
