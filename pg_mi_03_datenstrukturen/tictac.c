32ab75a1f00512a94121012fe993f6668d947db9
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2016-11-10 05:40:12 -0800

    Fertige in der Vorlesung erarbeitete Lösung

--- pg_mi_03_datenstrukturen/tictac.c
+++ pg_mi_03_datenstrukturen/tictac.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 #define EMPTY 0
+#define SPIELER1 1
+#define SPIELER2 2
 #define MAX 3
 
 int main(void) {
+  int spieler = SPIELER1;
   int xvalue = 0, yvalue = 0;
-  _Bool is_spielzug_valid = 0;
   int spielfeld[MAX][MAX];
  
   // Felder des Spielfelds mit EMPTY vorbelegen
@@ -15,16 +17,79 @@ int main(void) {
     }
   }
 
-  do {
-    // Spielzug erfragen
-    printf("X-Koordinate: ");
-    scanf("%d", &xvalue);
-    printf("Y-Koordinate: ");
-    scanf("%d", &yvalue);
-  } while(xvalue >= 0 && yvalue >= 0 && xvalue < MAX && yvalue < MAX);
+  _Bool is_zug_invalid = 0;
+  for(int z = 0; z < 9; z++) {
+    printf("Spieler: %d\n", spieler);
+
+    do {
+      // Spielzug erfragen
+      printf("X-Koordinate: ");
+      scanf("%d", &xvalue);
+      printf("Y-Koordinate: ");
+      scanf("%d", &yvalue);
+
+      is_zug_invalid = (xvalue < 0 || xvalue >= MAX)
+         || (yvalue < 0 || yvalue >= MAX);
+
+      if(is_zug_invalid) {
+        printf("Ihr Zug war ungültig, bitte versuchen Sie es erneut\n");
+      } 
+    } while(is_zug_invalid);
   
-  // Zum Testen:
-  printf("(%d:%d)\n", xvalue, yvalue);
+    // Zum Testen:
+    printf("(%d:%d)\n", xvalue, yvalue);
+
+    // Spielzug ins Spielfeld schreiben
+    spielfeld[xvalue][yvalue] = spieler;
+
+    // Spielfeld ausgeben
+    for(int i = 0; i < MAX; i++) {
+      for(int j = 0; j < MAX; j++) {
+        printf("%d ", spielfeld[j][i]);
+      }
+      printf("\n");
+    }
+
+    // Senkrechte prüfen
+    for(int i = 0; i < MAX; i++) {
+      if(spielfeld[i][0] != 0 
+         && spielfeld[i][0] == spielfeld[i][1] 
+         && spielfeld[i][0] == spielfeld[i][2]) {
+        printf("Spieler %d hat gewonnen\n", spieler);
+        return 0;
+      } 
+    }
+
+    // Wagrechte prüfen
+    for(int i = 0; i < MAX; i++) {
+      if(spielfeld[0][i] != 0
+         && spielfeld[0][i] == spielfeld[1][i]
+         && spielfeld[0][i] == spielfeld[2][i]) {
+        printf("Spieler %d hat gewonnen\n", spieler);
+        return 0;
+      } 
+    }
+
+    if(spielfeld[0][0] != 0
+       && spielfeld[0][0] == spielfeld[1][1]
+       && spielfeld[0][0] == spielfeld[2][2]) {
+      printf("Spieler %d hat gewonnen\n", spieler);
+      return 0;
+    }
+
+    if(spielfeld[0][2] != 0
+       && spielfeld[0][2] == spielfeld[1][1]
+       && spielfeld[0][2] == spielfeld[2][0]) {
+      printf("Spieler %d hat gewonnen\n", spieler);
+      return 0;
+    }
+
+    if(spieler == SPIELER1) {
+      spieler = SPIELER2;
+    } else {
+      spieler = SPIELER1;
+    }
+  }
 
   return 0;
 }
