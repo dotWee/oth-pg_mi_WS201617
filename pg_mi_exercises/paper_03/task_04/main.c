cb5d49b650605b2c4ff58deed75e518fc66655a3
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-11-22 13:58:06 +0100

    Update solution for task 04 paper 03: Check if args suit for substring-check

--- pg_mi_exercises/paper_03/task_04/main.c
+++ pg_mi_exercises/paper_03/task_04/main.c
@@ -16,8 +16,17 @@ int count_substring(const char *string, const char *substring) {
 
 int main(int argc, const char *argv[]) {
 
-    int ergebnis = count_substring(argv[2], argv[1]);
-    printf("Ergebnis: %d", ergebnis);
+    // Check args
+    int length_substring = strlen(argv[1]);
+    int length_orgstring = strlen(argv[2]);
 
-    return 0;
+    // Wenn die Teilzeichenkette größer als die Zeichenkette ist...
+    if (length_substring > length_orgstring) {
+        return -1;
+    } else {
+        int ergebnis = count_substring(argv[2], argv[1]);
+        printf("Ergebnis: %d", ergebnis);
+
+        return 0;
+    }
 }
