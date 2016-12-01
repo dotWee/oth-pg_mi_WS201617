72d91b07a0572599fd454739107f4bd187c3cf62
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2016-12-01 11:08:46 +0100

    Update solution for task 06 paper 05: Test values for negative and null exponents

--- pg_mi_exercises/paper_03/task_05/libs/functions_test.c
+++ pg_mi_exercises/paper_03/task_05/libs/functions_test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "functions.h"
 
+_Bool isBetween(double start, double end, double value) {
+    return start <= value && value <= end;
+}
+
 int test_my_sqrt(int testwert, double erwartung) {
     int error = 0;
 
@@ -9,9 +13,9 @@ int test_my_sqrt(int testwert, double erwartung) {
     double resultat_minus_abweichung = resultat - MAX_ABWEICHUNG;
 
     // Wenn das Resultat aus der Funktion ungleich des erwarteten Werts ist...
-    if (resultat_plus_abweichung <= erwartung && erwartung <= resultat_minus_abweichung) {
+    if (isBetween(resultat_minus_abweichung, resultat_plus_abweichung, erwartung)) {
         // Fehler
-        printf("Fehler bei my_sqrt: Resultat %lf aus Testwert %d ungleich der Erwartung %lf", resultat, testwert,
+        printf("Fehler bei my_sqrt: Resultat %lf aus Testwert %d ungleich der Erwartung %lf\n", resultat, testwert,
                erwartung);
         error += 1;
     }
@@ -27,7 +31,7 @@ int test_my_pow(int testwert, double erwartung) {
     // Wenn das Resultat aus der Funktion ungleich des erwarteten Werts ist...
     if (erwartung != resultat) {
         // Fehler
-        printf("Fehler bei my_pow: Resultat %lf aus Testwert %d ungleich der Erwartung %lf", resultat, testwert,
+        printf("Fehler bei my_pow: Resultat %lf aus Testwert %d ungleich der Erwartung %lf\n", resultat, testwert,
                erwartung);
         error += 1;
     }
@@ -43,10 +47,20 @@ int main(int argc, const char *argv[]) {
     double erwartung_sqrt = 3;
     errors += test_my_sqrt(testwert_sqrt, erwartung_sqrt);
 
-    // Teste my_pow Funktion
-    int testwert_pow = 2;
-    double erwartung_pow = 4.000000;
-    errors += test_my_pow(testwert_pow, erwartung_pow);
+    // Teste my_pow Funktion POSITIV
+    int testwert_pow_positiv = 2;
+    double erwartung_pow_positiv = 4.000000;
+    errors += test_my_pow(testwert_pow_positiv, erwartung_pow_positiv);
+
+    // Teste my_pow Funktion NEGATIV
+    int testwert_pow_negativ = -2;
+    double erwartung_pow_negativ = 0.250000;
+    errors += test_my_pow(testwert_pow_negativ, erwartung_pow_negativ);
+
+    // Teste my_pow Funktion NULL
+    int testwert_pow_null = 0;
+    double erwartung_pow_null = 1.000000;
+    errors += test_my_pow(testwert_pow_null, erwartung_pow_null);
 
     return errors;
 }
