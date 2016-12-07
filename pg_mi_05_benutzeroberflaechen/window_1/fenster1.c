f1bc126cd3d805a979fd34c8e860fe9cc49567f4
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2016-12-07 02:43:24 -0800

    Endstand nach der Vorlesung von 07.12.2016

--- pg_mi_05_benutzeroberflaechen/window_1/fenster1.c
+++ pg_mi_05_benutzeroberflaechen/window_1/fenster1.c
@@ -1,12 +1,7 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
 
 void on_destroy(GtkWidget *src, gpointer data) {
     gtk_main_quit();
-}
-
-void on_button_click(GtkWidget *src, gpointer data) {
-    printf("Button wurde geklickt\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -14,18 +9,11 @@ int main(int argc, char *argv[]) {
 
     // Hier kommt unser Fenstercode rein
     GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    GtkWidget* button;
-    button = gtk_button_new_with_label("Schließen2");
-
     g_signal_connect(window, "destroy", G_CALLBACK(on_destroy), NULL);
-    g_signal_connect(button, "clicked", G_CALLBACK(on_button_click), NULL);
 
     gtk_window_set_title(GTK_WINDOW(window), "Unser erstes Fenster");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 400);
 
-    gtk_container_add(GTK_CONTAINER(window), button);
-
-    gtk_widget_show(button);
     gtk_widget_show(window);
 
     gtk_main();
