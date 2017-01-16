650544743922fa2e0be47c7802d9578820b334a9
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2017-01-16 08:30:16 -0800

    ups, da war noch ein Test von der Fehleranalyse mit drin

--- uebung/stringstellen/stringstellen.c
+++ uebung/stringstellen/stringstellen.c
@@ -5,7 +5,7 @@ int anzahl_treffer(char* haystack, char* needle) {
     int counter = 0;
     int len = strlen(haystack);
     for(int i = 0; i < len; i++) {
-        if(strncmp(needle, haystack, strlen(needle)-1) == 0) {
+        if(strncmp(needle, haystack, strlen(needle)) == 0) {
             counter += 1;
         }
         haystack++;
