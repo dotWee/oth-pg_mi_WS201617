96143ddf82a197ed4fe401eb87d79e9f30258a65
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-01 16:02:24 +0100

    Update paper 01 task 06 solution

--- pg_mi_exercises/paper_01/task_06/main.c
+++ pg_mi_exercises/paper_01/task_06/main.c
@@ -1,35 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int zahl = 0;
-    printf("Zahl: ");
-    scanf("%d", &zahl);
+#define MAX_LENGTH 100
 
-    int istPrimzahl = -1;
+int lese_zahl() {
+
+    // Definiere Ziele
+    char *ziel = malloc(sizeof(char) * MAX_LENGTH);
+    fgets(ziel, MAX_LENGTH, stdin);
+
+    // Umwandlung in Zahl
+    int zahl = atoi(ziel);
+
+    // Speicher freigeben
+    free(ziel);
+
+    // Zahl zurückgeben
+    return zahl;
+}
+
+/**
+ * Diese Funktion checkt ob eine Zahl eine Primzahl ist oder nicht
+ *
+ * @param zahl Die überprüft werden soll
+ * @return 0 wenn falsch, 1 wenn wahr
+ */
+int ist_primzahl(int zahl) {
+
     if (zahl <= 1) {
-        // Keine Primzahl
-        istPrimzahl = 0;
+        return 0;
     }
 
     if (zahl % 2 == 0 && zahl > 2) {
-        // Keine Primzahl
-        istPrimzahl = 0;
+        return 0;
     }
 
     for (int i = 3; i < zahl / 2; i += 2) {
         if (zahl % i == 0) {
-            // Keine Primzahl
-            istPrimzahl = 0;
+            return 0;
         }
     }
 
-    if (istPrimzahl == -1) {
-        printf("\nZahl ist eine Primzahl");
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+
+    // Zahl einlesen
+    int zahl = lese_zahl();
+
+    // Überprüfen
+    int istPrimzahl = ist_primzahl(zahl);
+
+    // Ausgeben
+    if (istPrimzahl == 0) {
+        printf("Ist keine Primzahl");
+    } else {
+        printf("Ist eine Primzahl");
     }
 
-    if (istPrimzahl == 0) {
-        printf("\nZahl ist keine Primzahl");
-    }
+    // Optional:
+    //printf("Ist Primzahl: %s", istPrimzahl ? "Wahr" : "Falsch");
 
     return 0;
 }
