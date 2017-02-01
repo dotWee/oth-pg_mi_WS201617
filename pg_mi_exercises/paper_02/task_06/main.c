b35ca850530279e9346796a39b2526a72c110565
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-01 17:46:26 +0100

    Update paper 02 task 06 solution

--- pg_mi_exercises/paper_02/task_06/main.c
+++ pg_mi_exercises/paper_02/task_06/main.c
@@ -2,50 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define CHOICE_ENCRYPT 0
-#define CHOICE_DECRYPT 1
+#define MAX_LENGTH 100
 
-int main(int argc, const char *argv[]) {
+#define ENTSCHLUESSELN 0
+#define VERSCHLUESSELN 1
 
-    int CHOICE = 0;
+int lese_zahl() {
 
-    if (argc > 3 || argc < 3) {
-        printf("Parameter fehlen!\n");
-        return -1;
+    // Definiere Ziele
+    char *ziel = malloc(sizeof(char) * MAX_LENGTH);
+    fgets(ziel, MAX_LENGTH, stdin);
+
+    // Umwandlung in Zahl
+    int zahl = atoi(ziel);
+
+    // Speicher freigeben
+    free(ziel);
+
+    // Zahl zurückgeben
+    return zahl;
+}
+
+
+void entschluesseln(char *string, int n) {
+    for (int i = 0; i < strlen(string); ++i) {
+        int asciiwert = string[i];
+        string[i] = asciiwert - n;
+    }
+}
+
+void verschluesseln(char *string, int n) {
+    for (int i = 0; i < strlen(string); ++i) {
+        int asciiwert = string[i];
+        string[i] = asciiwert + n;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    printf("Gib 1 für verschlüsseln und 0 fur entschlüsseln ein: ");
+    int wahl = lese_zahl();
+
+    printf("Gib einen Text ein, der verarbeitet werden soll: ");
+    char *string = malloc(sizeof(char) * MAX_LENGTH);
+    fgets(string, MAX_LENGTH, stdin);
+
+    printf("Gib eine Zahl ein, mit der verschlüsselt wurde / verschlüsselt werden soll: ");
+    int n = lese_zahl();
+
+    switch (wahl) {
+
+        case VERSCHLUESSELN:
+            verschluesseln(string, n);
+            break;
+
+        case ENTSCHLUESSELN:
+            entschluesseln(string, n);
+            break;
+
+        default:
+            printf("Fehler");
+            return 1;
     }
 
-    printf("Bitte wähle eine Aktion:\n");
-    printf("[0] Text verschlüsseln\n");
-    printf("[1] Text entschlüsseln\n");
-    scanf("%d", &CHOICE);
-
-    if (CHOICE != CHOICE_ENCRYPT && CHOICE != CHOICE_DECRYPT) {
-        printf("Aktion existiert nicht!\n");
-        return -2;
-    }
-
-    // Anzahl der Stufen die in der ASCII-Tabelle gesprungen werden sollen
-    int amount = atoi(argv[1]);
-
-    // Für jeden Buchstaben aus dem 2. String-Parameter
-    for (int i = 0; i < strlen(argv[2]); ++i) {
-        char originalChar = argv[2][i];
-
-        int asciiValue = originalChar;
-
-        // "Verschlüssle" Char
-        if (CHOICE == CHOICE_ENCRYPT) {
-            asciiValue = asciiValue + amount;
-        }
-
-        if (CHOICE == CHOICE_DECRYPT) {
-            asciiValue = asciiValue - amount;
-        }
-
-        char encryptedChar = asciiValue;
-
-        printf("%c", encryptedChar);
-    }
+    printf("\n\nResultat: %s", string);
+    free(string);
 
     return 0;
 }
