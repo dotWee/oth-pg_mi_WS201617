2683634e733ba8c8d775470ab3b336937ce9d6fd
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-01 15:28:18 +0100

    Update paper 01 task 03 solution

--- pg_mi_exercises/paper_01/task_03/main.c
+++ pg_mi_exercises/paper_01/task_03/main.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int main(void) {
-    int kathete1, kathete2;
-    double hypotenuse;
+#define MAX_LENGTH 100
 
+int lese_zahl() {
+
+    // Definiere Ziele
+    char *ziel = malloc(sizeof(char) * MAX_LENGTH);
+    fgets(ziel, MAX_LENGTH, stdin);
+
+    // Umwandlung in Zahl
+    int zahl = atoi(ziel);
+
+    // Speicher freigeben
+    free(ziel);
+
+    // Zahl zurückgeben
+    return zahl;
+}
+
+int main(int argc, char *argv[]) {
+
+    // Fragen nach Kathete1
     printf("Kathete1: ");
-    scanf("%d", &kathete1);
+    int kathete1 = lese_zahl();
 
+    // Fragen nach Kathete2
     printf("Kathete2: ");
-    scanf("%d", &kathete2);
+    int kathete2 = lese_zahl();
 
-    hypotenuse = (kathete1 * kathete1) + (kathete2 * kathete2);
-    hypotenuse = sqrt(hypotenuse);
+    // Berechnen a^2*b^2=c^2
+    kathete1 = kathete1 * kathete1;
+    kathete2 = kathete2 * kathete2;
+    double hypotenuse = sqrt(kathete1 + kathete2);
 
-    printf("---------");
-    printf("\nHypotenuse: %lf", hypotenuse);
+    // Hypotenuse ausgeben
+    printf("Hypotenuse: %lf", hypotenuse);
 
+    // Erfolg zurückgeben
     return 0;
 }
