25bab98da9c33b1eca01085a9439a216fed9a70d
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-01 15:22:43 +0100

    Update paper 01 task 02 solution

--- pg_mi_exercises/paper_01/task_02/main.c
+++ pg_mi_exercises/paper_01/task_02/main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int laenge = 0;
-    int breite = 0;
+#define MAX_LENGTH 100
 
-    int flaeche;
+int lese_zahl() {
 
+    // Definiere Ziele
+    char *ziel = malloc(sizeof(char) * MAX_LENGTH);
+    fgets(ziel, MAX_LENGTH, stdin);
+
+    // Umwandlung in Zahl
+    int zahl = atoi(ziel);
+
+    // Speicher freigeben
+    free(ziel);
+
+    // Zahl zurückgeben
+    return zahl;
+}
+
+int main(int argc, char *argv[]) {
+
+    // Fragen nach Länge
     printf("Länge: ");
-    scanf("%d", &laenge);
+    int laenge = lese_zahl();
 
-    printf("Breite: ");
-    scanf("%d", &breite);
+    // Fragen nach Höhe
+    printf("Höhe: ");
+    int hoehe = lese_zahl();
 
-    flaeche = laenge * breite;
-    printf("---------");
-    printf("\nFläche: %d", flaeche);
+    // Berechnen
+    int flaeche = laenge * hoehe;
+    printf("---------\n");
+    printf("Fläche: %d", flaeche);
 
     return 0;
 }
