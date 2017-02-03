3218f4d163be46530e3796487ddfdee45c77cbc0
Lukas Wolfsteiner <lukas@wolfsteiner.de>
2017-02-03 06:49:16 +0100

    Update paper 02 task 01 solution

--- pg_mi_exercises/paper_02/task_01/main.c
+++ pg_mi_exercises/paper_02/task_01/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LENGTH 100
+#define ERROR_ASTREIHEN 1
 
 void print_leerzeichen(int anzahl) {
     for (int i = 0; i < anzahl; ++i) {
@@ -15,13 +15,22 @@ void print_stern(int anzahl) {
     }
 }
 
-int lese_zahl() {
-
-    // Ziel, in das die Eingabe gelesen werden soll
-    char *ziel = malloc(sizeof(char) * MAX_LENGTH);
+void print_stamm() {
+    for (int i = 0; i < 3; ++i) {
+        printf("|");
+    }
 }
 
 int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Fehler: Keine Anzahl der Astreihen gegeben");
+        return ERROR_ASTREIHEN;
+    }
+
+    // Astreihen auslesen
+    int astreihen = atoi(argv[1]);
+    printf("Astreihen: %d", astreihen);
 
     return 0;
 }
